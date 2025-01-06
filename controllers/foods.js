cce4470b71d4fe300901ac377e8f3eb27d975d01
Cesar Preza <cpreza93@yahoo.com>
2025-01-05 19:02:15 -0800

    adds new food route

--- controllers/foods.js
+++ controllers/foods.js
@@ -4,11 +4,20 @@ const router = express.Router();
 const User = require('../models/user.js');
 
 router.get('/',  (req, res) => {
-    res.render('/foods/index.js');
+    res.render('foods/index.ejs');
+});
+
+router.get('/users/:userId/foods', (req, res) => {
+    res.render('foods/index.ejs')
 });
 
 router.get('/users/:userId/foods/new', (req, res) => {
-    res.render('foods/new.ejs')
-})
+    res.render('foods/new.ejs');
+});
+
+
+
+
+
 
 module.exports = router;
