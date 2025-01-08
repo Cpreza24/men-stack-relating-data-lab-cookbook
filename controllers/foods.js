f58a238a64faa08ee9610d36749d68669577e1bf
Cesar Preza <cpreza93@yahoo.com>
2025-01-07 18:23:22 -0800

    adds index and new route

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,21 +3,26 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-router.get('/',  (req, res) => {
+router.get('/', (req, res) => {
     res.render('foods/index.ejs');
 });
 
-router.get('/users/:userId/foods', (req, res) => {
-    res.render('foods/index.ejs')
+//INDEX
+router.get('/foods', (req, res) => {
+    try {
+        res.render('foods/index.ejs');
+    } catch (error) {
+        console.error(error.message, { message: 'server error' });
+    }
 });
 
-router.get('/users/:userId/foods/new', (req, res) => {
-    res.render('foods/new.ejs');
+//NEW
+router.get('/foods/new', async (req, res) => {
+    try {
+        res.render('foods/new.ejs');
+    } catch (error) {
+        console.error(error.message, { message: 'server error' });
+    }
 });
-
-
-
-
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -13,47 +13,47 @@ const foodsController = require('./controllers/foods.js');
 const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
 
-
 const port = process.env.PORT ? process.env.PORT : '3000';
 
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // app.use(morgan('dev'));
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
 );
 
 app.use(passUserToView);
 app.use('/auth', authController);
 app.use(isSignedIn);
-app.use('/users/:userId/foods', foodsController);
+app.use('/users/:userId', foodsController);
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
+    res.render('index.ejs', {
+        user: req.session.user,
+    });
 });
 
 app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
+    if (req.session.user) {
+        res.send(`Welcome to the party ${req.session.user.username}.`);
+    } else {
+        res.send('Sorry, no guests allowed.');
+    }
 });
 
 app.use('/auth', authController);
 
 app.listen(port, () => {
-  console.log(`The express app is ready on port ${port}!`);
+    console.log(`The express app is ready on port ${port}!`);
 });
